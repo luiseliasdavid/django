e2b75178bf6f1bc81b0acb52c3afb251b12e176a
luiseliasdavid <luis.elias.david@gmail.com>
2023-06-09 21:49:46 -0300

    home view

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+
+def home(request):
+    """vista home"""
+    return HttpResponse("<h1>Bienvenidos a mi blog</h1>")
